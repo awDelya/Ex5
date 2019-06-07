39f59aead81305f4ee6c2eb3263118e16633ba80
Овчинникова Аделина <deley@mail.ru>
2019-06-07 14:38:45 +0500

    Create project

---
+++ Find max in matrix/Find max in matrix/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_max_in_matrix
+{
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
